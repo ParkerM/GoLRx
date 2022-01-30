908d4ba6c4bb98f214b90941a995b29ee6c4c8af
Parker Mauney <parkermauney@gmail.com>
2022-01-29 18:43:00 -0600

    Add snowpack

---
+++ snowpack.config.mjs
@@ -0,0 +1,25 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+const snowpackUserConfig = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default snowpackUserConfig;
