0f71ac85afbee5df0d3a3810434da696ad1e31f7
Parker Mauney <parkermauney@gmail.com>
2022-02-19 22:09:21 -0600

    Add list of WellKnownRules and default to GoL

--- src/lib/cell.spec.js
+++ src/lib/cell.spec.js
@@ -41,62 +41,72 @@ describe('Cells', () => {
     ticker = new Subject();
     changeEmitter = new Subject();
     stopSignal = new Subject();
-    cell = new Cell(ticker.asObservable(), 1, 1, changeEmitter);
   });
 
-  it('Any live cell with fewer than two live neighbours dies, as if by underpopulation', (done) => {
-    cell.state = State.ALIVE;
-    setupNeighbors(cell, [State.ALIVE, State.DEAD]);
-
-    cell.listenUntil(stopSignal).subscribe({
-      next: expectNext(State.DEAD, done),
+  describe('Game of Life', () => {
+    beforeEach(() => {
+      cell = new Cell(ticker.asObservable(), 1, 1, changeEmitter);
     });
-    ticker.next(void 0);
-    stopSignal.next(void 0);
-  });
 
-  it('Any live cell with two live neighbours lives on to the next generation', (done) => {
-    cell.state = State.ALIVE;
-    setupNeighbors(cell, [State.ALIVE, State.ALIVE]);
+    it('Any live cell with fewer than two live neighbours dies, as if by underpopulation', (done) => {
+      cell.state = State.ALIVE;
+      setupNeighbors(cell, [State.ALIVE, State.DEAD]);
 
-    cell.listenUntil(stopSignal).subscribe({
-      next: expectNext(State.ALIVE, done),
+      cell.listenUntil(stopSignal).subscribe({
+        next: expectNext(State.DEAD, done),
+      });
+      ticker.next(void 0);
+      stopSignal.next(void 0);
     });
-    ticker.next(void 0);
-    stopSignal.next(void 0);
-  });
 
-  it('Any live cell with three live neighbours lives on to the next generation', (done) => {
-    cell.state = State.ALIVE;
-    setupNeighbors(cell, [State.ALIVE, State.ALIVE, State.ALIVE]);
+    it('Any live cell with two live neighbours lives on to the next generation', (done) => {
+      cell.state = State.ALIVE;
+      setupNeighbors(cell, [State.ALIVE, State.ALIVE]);
 
-    cell.listenUntil(stopSignal).subscribe({
-      next: expectNext(State.ALIVE, done),
+      cell.listenUntil(stopSignal).subscribe({
+        next: expectNext(State.ALIVE, done),
+      });
+      ticker.next(void 0);
+      stopSignal.next(void 0);
     });
-    ticker.next(void 0);
-    stopSignal.next(void 0);
-  });
 
-  it('Any live cell with more than three live neighbours dies, as if by overpopulation', (done) => {
-    cell.state = State.ALIVE;
-    setupNeighbors(cell, [State.ALIVE, State.ALIVE, State.ALIVE, State.ALIVE]);
+    it('Any live cell with three live neighbours lives on to the next generation', (done) => {
+      cell.state = State.ALIVE;
+      setupNeighbors(cell, [State.ALIVE, State.ALIVE, State.ALIVE]);
 
-    cell.listenUntil(stopSignal).subscribe({
-      next: expectNext(State.DEAD, done),
+      cell.listenUntil(stopSignal).subscribe({
+        next: expectNext(State.ALIVE, done),
+      });
+      ticker.next(void 0);
+      stopSignal.next(void 0);
     });
-    ticker.next(void 0);
-    stopSignal.next(void 0);
-  });
 
-  it('Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction', (done) => {
-    cell.state = State.DEAD;
-    setupNeighbors(cell, [State.ALIVE, State.ALIVE, State.ALIVE]);
+    it('Any live cell with more than three live neighbours dies, as if by overpopulation', (done) => {
+      cell.state = State.ALIVE;
+      setupNeighbors(cell, [
+        State.ALIVE,
+        State.ALIVE,
+        State.ALIVE,
+        State.ALIVE,
+      ]);
 
-    cell.listenUntil(stopSignal).subscribe({
-      next: expectNext(State.ALIVE, done),
+      cell.listenUntil(stopSignal).subscribe({
+        next: expectNext(State.DEAD, done),
+      });
+      ticker.next(void 0);
+      stopSignal.next(void 0);
     });
-    ticker.next(void 0);
-    stopSignal.next(void 0);
+
+    it('Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction', (done) => {
+      cell.state = State.DEAD;
+      setupNeighbors(cell, [State.ALIVE, State.ALIVE, State.ALIVE]);
+
+      cell.listenUntil(stopSignal).subscribe({
+        next: expectNext(State.ALIVE, done),
+      });
+      ticker.next(void 0);
+      stopSignal.next(void 0);
+    });
   });
 });
 
--- src/lib/rule.js
+++ src/lib/rule.js
@@ -28,4 +28,22 @@ class Rule {
   }
 }
 
-export { Rule };
+class WellKnownRules {
+  static REPLICATOR = new Rule('B1357/S1357');
+  static FREDKIN = new Rule('B1357/S02468');
+  static SEEDS = new Rule('B2/S');
+  static LIVE_FREE_OR_DIE = new Rule('B2/S0');
+  static LIFE_WITHOUT_DEATH = new Rule('B3/S012345678');
+  static FLOCK = new Rule('B3/S12');
+  static MAZECTRIC = new Rule('B3/S1234');
+  static MAZE = new Rule('B3/S12345');
+  static GAME_OF_LIFE = new Rule('B3/S23');
+  static TWO_BY_TWO = new Rule('B36/S125');
+  static HIGH_LIFE = new Rule('B36/S23');
+  static MOVE = new Rule('B368/S245');
+  static DAY_AND_NIGHT = new Rule('B3678/34678');
+  static DRY_LIFE = new Rule('B37/S23');
+  static PEDESTRIAN_LIFE = new Rule('B38/S23');
+}
+
+export { Rule, WellKnownRules };
