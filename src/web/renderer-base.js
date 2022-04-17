a32cb5b1d7d273b165f0080df5abf13f12e1073d
Parker Mauney <parkermauney@gmail.com>
2022-04-17 16:16:29 -0500

    RendererBase doc polish

--- src/web/renderer-base.js
+++ src/web/renderer-base.js
@@ -1,7 +1,15 @@
+import { Subject } from 'rxjs';
+
 /**
  * @abstract
  */
 class RendererBase {
+  /**
+   * Subject that emits [x, y, isActive] when a cell is clicked.
+   * @type {Subject<[number, number, State]>}
+   */
+  cellToggled = new Subject();
+
   /**
    * Draws a grid of cells to a child grid container.
    *
@@ -11,8 +19,10 @@ class RendererBase {
   drawGrid() {}
 
   /**
+   * Toggles the state of a visible cell.
+   *
    * @abstract
-   * @param event {MouseEvent}
+   * @param event {MouseEvent} - the event that triggered this action
    * @param x {number} - the relative x coordinate of this cell in the grid
    * @param y {number} - the relative y coordinate of this cell in the grid
    */
@@ -29,6 +39,27 @@ class RendererBase {
   setCellState(x, y, state) {}
 
   /**
+   * Sets the state of 0 or more cells to active/alive for the
+   * given coordinate pairs. Useful for loading presets.
+   *
+   * @example
+   * // load preset
+   * const glider = [
+   *   [0, 2],
+   *   [1, 3],
+   *   [2, 1],
+   *   [2, 2],
+   *   [2, 3],
+   * ];
+   * renderer.activateCells(glider);
+   *
+   * // result (assuming row-major ordering)
+   * //    . . . .
+   * //    . . # .
+   * //    # . # .
+   * //    . # # .
+   * //    . . . .
+   *
    * @abstract
    * @param coords {[number, number][]} x,y pairs to activate
    */
